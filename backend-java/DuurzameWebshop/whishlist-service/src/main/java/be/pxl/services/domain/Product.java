e2465047c6d83402d082c9aca34e56b712b0f9d4
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-05 00:19:10 +0200

    initial version wishlist MS

---
+++ backend-java/DuurzameWebshop/whishlist-service/src/main/java/be/pxl/services/domain/Product.java
@@ -0,0 +1,9 @@
+package be.pxl.services.domain;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+    private boolean available = true;
+}
--- backend-java/DuurzameWebshop/whishlist-service/src/main/java/be/pxl/services/domain/Whishlist.java
+++ backend-java/DuurzameWebshop/whishlist-service/src/main/java/be/pxl/services/domain/Whishlist.java
@@ -1,13 +1,12 @@
 package be.pxl.services.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Data
@@ -18,8 +17,8 @@ public class Whishlist {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
-    private Long shoppingcartId;
+    @Transient
+    private List<Product> products;
     private Long userId;
 
 }
