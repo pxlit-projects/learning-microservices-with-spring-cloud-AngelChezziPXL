11db0705c11e59e368b36429641cbb3c0473dafc
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-04 17:38:21 +0200

    files deleted because the productCatalogus MS has these

--- backend-java/DuurzameWebshop/winkelwagen-service/src/main/java/be/pxl/services/winkelwagen/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package be.pxl.services.winkelwagen.repository;
-
-import be.pxl.services.winkelwagen.domain.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-//@Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
