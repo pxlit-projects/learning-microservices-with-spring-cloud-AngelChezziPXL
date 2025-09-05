fb7edb2cd53ac43989b38cd40e3bce09619d5d81
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-05 02:29:35 +0200

    Added config-service and updated other services to use it

---
+++ backend-java/DuurzameWebshop/config-service/src/main/java/be/pxl/services/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * ConfigServerApplication!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
