a2cde8cbc82701596af8de788fe2fa38d3bef5fe
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-05 17:36:41 +0200

    initial version with endpoint to retrieve all wishlists -> TODO: update to use token for one specific whishlist

--- backend-java/DuurzameWebshop/whishlist-service/src/main/java/be/pxl/services/whishlist/controller/WhishlistController.java
+++ backend-java/DuurzameWebshop/whishlist-service/src/main/java/be/pxl/services/whishlist/controller/WhishlistController.java
@@ -3,8 +3,10 @@ package be.pxl.services.whishlist.controller;
 import be.pxl.services.whishlist.domain.Whishlist;
 import be.pxl.services.whishlist.services.IWishlistService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,7 +16,13 @@ import java.util.List;
 @RequestMapping("/api/whishlist")
 public class WhishlistController {
 
-    private IWishlistService wishlistService;
+    private final IWishlistService wishlistService;
+
+    @GetMapping("test")
+    @ResponseStatus(HttpStatus.OK)
+    public String getWhishlistTest() {
+        return "whishlist service is running.";
+    }
 
     @GetMapping
     public List<Whishlist> getWhishlist() {
