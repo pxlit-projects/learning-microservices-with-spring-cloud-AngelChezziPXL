bc5f2e18fde919f85d376dc88c080dbad73d14ca
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-06 11:34:36 +0200

    - removed unused imports
    - autowired rabbitMqService

--- backend-java/DuurzameWebshop/productcatalog-service/src/main/java/be/pxl/services/productcatalog/service/RabbitMqService.java
+++ backend-java/DuurzameWebshop/productcatalog-service/src/main/java/be/pxl/services/productcatalog/service/RabbitMqService.java
@@ -3,19 +3,19 @@ package be.pxl.services.productcatalog.service;
 import be.pxl.services.productcatalog.domain.dto.ProductQueueMessage;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class RabbitMqService implements IRabbitMqService{
     ObjectMapper mapper = new ObjectMapper();
-    @Autowired
-    private RabbitTemplate rabbitTemplate = new RabbitTemplate();
-    private final String ROUTE_KEY = "product-queue";
+    private final RabbitTemplate rabbitTemplate;
+    private final String QUEUE_NAME = "product-queue";
 
     public void sendMessageToQueue(ProductQueueMessage productQueueMessage) throws JsonProcessingException {
         String jsonString = mapper.writeValueAsString(productQueueMessage);
-        rabbitTemplate.convertAndSend(ROUTE_KEY, jsonString);
+        rabbitTemplate.convertAndSend(QUEUE_NAME, jsonString);
     }
 }
