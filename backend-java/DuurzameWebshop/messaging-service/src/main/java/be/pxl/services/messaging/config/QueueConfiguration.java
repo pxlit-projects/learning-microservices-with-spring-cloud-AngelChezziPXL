93ac289ac29ffb2583ca9402f5c6667f825272b9
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-06 11:42:11 +0200

    removed commented lines

--- backend-java/DuurzameWebshop/messaging-service/src/main/java/be/pxl/services/messaging/config/QueueConfiguration.java
+++ backend-java/DuurzameWebshop/messaging-service/src/main/java/be/pxl/services/messaging/config/QueueConfiguration.java
@@ -6,13 +6,20 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QueueConfiguration {
+
+//    @Bean
+//    public Queue myQueue() {
+////        return new Queue("myQueue", false);
+//        return new Queue("myQueue", true);
+//        //return new Queue("productcatalog-queue", false);
+//    }
     @Bean
-    public Queue myQueue() {
-        return new Queue("myQueue", false);
+      public Queue productQueue() {
+        return new Queue("product-queue", true);
     }
 
-    @Bean
-    public Queue productQueue() {
-        return new Queue("productcatalog-queue", false);
-    }
+//    @Bean
+//    public Queue productQueue() {
+//        return new Queue("productcatalog-queue", false);
+//    }
 }
