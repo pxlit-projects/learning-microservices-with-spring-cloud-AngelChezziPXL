81611ff1f9c43f05ceb644b504c83a872552a7af
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-06 11:33:13 +0200

    added

---
+++ backend-java/DuurzameWebshop/productcatalog-service/src/main/java/be/pxl/services/productcatalog/exception/AuthorizationException.java
@@ -0,0 +1,11 @@
+package be.pxl.services.productcatalog.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class AuthorizationException extends RuntimeException {
+    public AuthorizationException(String message) {
+        super(message);
+    }
+}
