3cb342a479399ae2cf9bbf8440b9a58f7ff76340
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-06 11:41:14 +0200

    endpoints added

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/controller/ShoppingCartController.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/controller/ShoppingCartController.java
@@ -1,30 +1,39 @@
 package be.pxl.services.shoppingcart.controller;
 
-import be.pxl.services.shoppingcart.controller.dto.ItemNewRequest;
+import be.pxl.services.shoppingcart.domain.dto.ItemNewRequest;
+import be.pxl.services.shoppingcart.domain.dto.ItemResponse;
+import be.pxl.services.shoppingcart.service.IShoppingCartService;
 import be.pxl.services.shoppingcart.service.ShoppingCartService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api/shoppingcart")
 @RequiredArgsConstructor
 public class ShoppingCartController {
-    private final ShoppingCartService shoppingCartService;
+    private final IShoppingCartService shoppingCartService;
+
+    @GetMapping("/test")
+    @ResponseStatus(HttpStatus.OK)
+    public String testEndpoint() {
+        return "Shoppingcart service is Ok";
+    }
 
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
-    public String sayHello() {
-        return "Get Ok";
+    public List<ItemResponse> getAllItemsWithProductDetails() {
+        return shoppingCartService.getAllItemsWithProductDetails();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("{id}")
+    @PostMapping("/{id}")
     public String AddItemToShoppingCart(@PathVariable Integer id, @RequestBody @Valid ItemNewRequest itemNewRequest) {
         return "Post ok";
-
     }
 
 }
