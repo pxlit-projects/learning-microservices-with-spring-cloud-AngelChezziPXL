20b3c1e8f4b9fb6c2692f6bde44c404a990bec42
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-06 11:38:02 +0200

    added client interface for synchronous communication via openfeign

---
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/ProductClient.java
@@ -0,0 +1,15 @@
+package be.pxl.services.shoppingcart.client;
+
+import be.pxl.services.shoppingcart.domain.dto.ProductRequest;
+import be.pxl.services.shoppingcart.domain.dto.ProductResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient(name="product-catalog")
+public interface ProductClient {
+    @GetMapping("/api/product")
+    List<ProductResponse> getAllProducts(@RequestBody ProductRequest productRequest);
+}
