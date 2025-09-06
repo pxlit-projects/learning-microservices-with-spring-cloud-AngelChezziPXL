04f610683cd83c2fd0573f56c6ba6a3e0bdb6d6a
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-06 10:59:57 +0200

    annotation added to exception

--- backend-java/DuurzameWebshop/logbook-service/src/main/java/be/pxl/services/logbook/exception/AuthorizationException.java
+++ backend-java/DuurzameWebshop/logbook-service/src/main/java/be/pxl/services/logbook/exception/AuthorizationException.java
@@ -1,5 +1,9 @@
 package be.pxl.services.logbook.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
 public class AuthorizationException extends RuntimeException {
     public AuthorizationException(String message) {
         super(message);
