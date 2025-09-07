1b5e7554d6e613df9b83916f2d421d362147847c
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-07 23:10:38 +0200

    new functionalities:  - to add, remove items  - to order and checkout shoppincart  - to publish shoppingcart to whishlist service

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/controller/ShoppingCartController.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/controller/ShoppingCartController.java
@@ -1,14 +1,12 @@
 package be.pxl.services.shoppingcart.controller;
 
 import be.pxl.services.shoppingcart.domain.dto.ItemNewRequest;
-import be.pxl.services.shoppingcart.domain.dto.ItemResponse;
-import be.pxl.services.shoppingcart.domain.dto.NewShoppingCartResponse;
 import be.pxl.services.shoppingcart.domain.dto.ProductResponse;
+import be.pxl.services.shoppingcart.domain.dto.ShoppingCartDto;
 import be.pxl.services.shoppingcart.domain.dto.ShoppingCartResponse;
 import be.pxl.services.shoppingcart.exception.AuthorizationException;
 import be.pxl.services.shoppingcart.service.IShoppingCartService;
 import jakarta.validation.Valid;
-import jakarta.ws.rs.PathParam;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,20 +43,55 @@ public class ShoppingCartController {
         LOG.info("Creating shoppingcart.");
         checkAuthorization(headers);
         long userId = Long.parseLong(headers.get("user_id"));
-        return shoppingCartService.createNewShoppingcart(userId);
+        return shoppingCartService.createNewShoppingcart(userId).toShoppingCartResponse();
     }
 
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{id}")
-    public void AddItemToShoppingCart(@RequestHeader Map<String, String> headers, @PathVariable long shoppingCartId, @RequestBody @Valid ItemNewRequest itemNewRequest) {
+    @PostMapping("/item")
+    public ShoppingCartResponse addItemToShoppingCart(@RequestHeader Map<String, String> headers, @RequestBody @Valid ItemNewRequest itemNewRequest) {
         LOG.info("Adding new item to shopping cart.");
         checkAuthorization(headers);
         long userId = Long.parseLong(headers.get("user_id"));
-        shoppingCartService.addItemToShoppingCart(userId, shoppingCartId, itemNewRequest);
+        return shoppingCartService.addItemToShoppingcart(userId, itemNewRequest).toShoppingCartResponse();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @DeleteMapping("/item/{shoppingCartId}/{itemId}")
+    public ShoppingCartResponse removeItemFromShoppingCart(@RequestHeader Map<String, String> headers, @PathVariable long shoppingCartId, @PathVariable long itemId) {
+        LOG.info("Removing item from shopping cart.");
+        checkAuthorization(headers);
+        ShoppingCartDto shoppingCartDto = shoppingCartService.removeItemByItemId(shoppingCartId, itemId);
+        return shoppingCartDto.toShoppingCartResponse();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping("/{shoppingCartId}")
+    public ShoppingCartResponse doOrder(@RequestHeader Map<String, String> headers, @PathVariable long shoppingCartId) {
+        LOG.info("Conferming shopping cart order.");
+        checkAuthorization(headers);
+        return shoppingCartService.doOrder(shoppingCartId).toShoppingCartResponse();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PutMapping("/{shoppingCartId}")
+    public ShoppingCartResponse payOrder(@RequestHeader Map<String, String> headers, @PathVariable long shoppingCartId) {
+        LOG.info("Checking out shopping cart.");
+        checkAuthorization(headers);
+        return shoppingCartService.checkOut(shoppingCartId).toShoppingCartResponse();
+    }
+
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    @PostMapping("/whishlist/{shoppingCartId}")
+    public void publishToWhishlist(@RequestHeader Map<String, String> headers, @PathVariable long shoppingCartId) {
+        LOG.info("Publishing shopping cart to whishlist.");
+        checkAuthorization(headers);
+        long userId = Long.parseLong(headers.get("user_id"));
+        shoppingCartService.publishShoppingCartToWhishlist(userId, shoppingCartId);
     }
 
     //PRIVATE HELPER METHODS
     private void checkAuthorization(Map<String, String> headers) {
+        LOG.info("Checking authorization.");
         String role = headers.get("role");
         long userId = headers.get("user_id") != null ? Long.parseLong(headers.get("user_id")): 0;
         if (!role.equalsIgnoreCase("admin")) {
@@ -70,6 +103,7 @@ public class ShoppingCartController {
             LOG.debug("User id cannot be null");
             throw new AuthorizationException("User id cannot be null or zero");
         }
+        LOG.info("Authorization successful.");
     }
 
 }
