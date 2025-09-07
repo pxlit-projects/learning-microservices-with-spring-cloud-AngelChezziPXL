ddad323b9f061ffcd2bb3e12d4ad08ca08eadd2e
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-07 22:10:58 +0200

    added new method to enable getById product

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/ProductClient.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/ProductClient.java
@@ -1,10 +1,8 @@
 package be.pxl.services.shoppingcart.client;
 
-import be.pxl.services.shoppingcart.domain.dto.ProductRequest;
 import be.pxl.services.shoppingcart.domain.dto.ProductResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
