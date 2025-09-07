e3658deeabc4cd245efaa7c67bc0719c4378b9da
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-07 23:07:29 +0200

    updated name

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/repository/IItemRepository.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/repository/IItemRepository.java
@@ -3,5 +3,5 @@ package be.pxl.services.shoppingcart.repository;
 import be.pxl.services.shoppingcart.domain.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IShoppingCartItemRepository extends JpaRepository<Item, Long> {
+public interface IItemRepository extends JpaRepository<Item, Long> {
 }
