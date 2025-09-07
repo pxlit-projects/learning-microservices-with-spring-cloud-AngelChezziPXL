2775b235b06ade1cdcf967d246ffef829d44f975
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-07 10:30:24 +0200

    added functionality to retrieve on userid

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/repository/IShoppingCartRepository.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/repository/IShoppingCartRepository.java
@@ -4,6 +4,9 @@ import be.pxl.services.shoppingcart.domain.ShoppingCart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
+    Optional<ShoppingCart> findByUserId(long userId);
 }
