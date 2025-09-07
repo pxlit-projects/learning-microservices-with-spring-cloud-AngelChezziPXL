db7745e9b86e30c095206f69926912d3abbbcbe5
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-07 22:10:35 +0200

    added new client to communicate synchronous with whishlist service

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/WhishListClient.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/WhishListClient.java
@@ -1,11 +1,11 @@
 package be.pxl.services.shoppingcart.client;
 
-import be.pxl.services.shoppingcart.domain.dto.WhishListResponse;
+import be.pxl.services.shoppingcart.domain.dto.WhishListDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @FeignClient("wishlist-service")
 public interface WhishListClient {
     @PostMapping("/api/whishlist")
-    WhishListResponse publishToWhishList();
+    WhishListDto publishToWhishList();
 }
