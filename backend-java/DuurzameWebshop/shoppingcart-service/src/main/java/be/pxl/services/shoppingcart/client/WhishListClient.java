dc708648cc43dde66b32b50ea079a6b61f840aa5
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-07 23:11:09 +0200

    method signature updated to accept shoppingCartResponse

--- backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/WhishListClient.java
+++ backend-java/DuurzameWebshop/shoppingcart-service/src/main/java/be/pxl/services/shoppingcart/client/WhishListClient.java
@@ -1,5 +1,6 @@
 package be.pxl.services.shoppingcart.client;
 
+import be.pxl.services.shoppingcart.domain.dto.ShoppingCartResponse;
 import be.pxl.services.shoppingcart.domain.dto.WhishListDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,5 +8,5 @@ import org.springframework.web.bind.annotation.PostMapping;
 @FeignClient("wishlist-service")
 public interface WhishListClient {
     @PostMapping("/api/whishlist")
-    WhishListDto publishToWhishList();
+    WhishListDto publishToWhishList(ShoppingCartResponse shoppingCartResponse);
 }
