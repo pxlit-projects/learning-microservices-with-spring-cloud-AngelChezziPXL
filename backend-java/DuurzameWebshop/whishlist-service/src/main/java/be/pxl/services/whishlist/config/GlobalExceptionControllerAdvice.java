96db2d569f3e899dd5be96c5c119186cb3c3d948
AngelChezziPXL <angel.chezzi@student.pxl.be>
2025-09-08 10:24:16 +0200

    Added exception handling

---
+++ backend-java/DuurzameWebshop/whishlist-service/src/main/java/be/pxl/services/whishlist/config/GlobalExceptionControllerAdvice.java
@@ -0,0 +1,67 @@
+package be.pxl.services.whishlist.config;
+
+import be.pxl.services.whishlist.controller.model.ApiError;
+import be.pxl.services.whishlist.exception.AuthorizationException;
+import be.pxl.services.whishlist.exception.ConflictException;
+import be.pxl.services.whishlist.exception.ResourceNotFoundException;
+import com.fasterxml.jackson.core.JsonParseException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionControllerAdvice {
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        ApiError error = new ApiError(HttpStatus.NOT_FOUND, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity
+                .status(error.getStatus())
+                .body(error);
+    }
+
+    @ExceptionHandler({ConflictException.class})
+    public ResponseEntity<Object> handleConflictException(ConflictException ex, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.CONFLICT;
+        ApiError error = new ApiError(status, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler({AuthorizationException.class})
+    public ResponseEntity<Object> handleAuthorizationException(AuthorizationException ex, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNAUTHORIZED;
+        ApiError error = new ApiError(status, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    ResponseEntity<Object> handleMethodArgumentNotValidException(MethodArgumentNotValidException methodArgumentNotValidException, HttpServletRequest request) {
+        String message = methodArgumentNotValidException
+                .getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining(", "));
+
+        ApiError error = new ApiError(
+                HttpStatus.BAD_REQUEST,
+                message,
+                request.getRequestURI()
+        );
+
+        return ResponseEntity
+                .status(error.getStatus())
+                .body(error);
+    }
+
+    @ExceptionHandler({JsonParseException.class})
+    public ResponseEntity<Object> handleJsonParseException(JsonParseException ex, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR;
+        ApiError error = new ApiError(status, ex.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(error);
+    }
+}
